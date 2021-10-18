0c866fe298463ff179a794f19aa3e8e9b0d7f99e
mariuszchucherko <mariuszchucherko@workhuman.com>
2021-10-18 09:16:45 +0100

    Commit after 39. Default Profile

--- src/main/java/pl/com/mcsystems/sfgdi/services/I18nSpanishService.java
+++ src/main/java/pl/com/mcsystems/sfgdi/services/I18nSpanishService.java
@@ -3,7 +3,7 @@ package pl.com.mcsystems.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES","default"})
 @Service("i18mService")
 public class I18nSpanishService implements GreetingService{
     @Override
